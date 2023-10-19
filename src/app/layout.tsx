5525c2f2004370d480782ac9391001490d6ab506
gibify <gibifyofficial@gmail.com>
2023-10-18 21:29:54 -0300

    dark and light mode

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from './theme-provider'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Next13 | Next-Themes | TailwindCSS',
+  description: 'A dark and light mode using Next13, Next-Themes and TailwindCSS',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { ThemeSwitcher } from '@/components/theme-switcher'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col p-4">
+      <ThemeSwitcher />
+    </main>
+  )
+}
